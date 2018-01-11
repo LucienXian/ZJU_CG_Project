7eb76b730df0c2c3b2b20e93ed32e3a17758c4df
QuanQuanoooooo <33426645+QuanQuanoooooo@users.noreply.github.com>
2018-01-11 16:41:15 +0800

    Add files via upload
    更新screenshot

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,12 +1,14 @@
 function main(){
 	canvas = document.getElementById('webgl');
+
 	//get canvas element
 	if(!canvas){
 		console.log('Failed to retrieve the <canvas> element');
 		return;
 	}
 	
-	gl = getWebGLContext(canvas);
+	//gl = getWebGLContext(canvas);
+    gl = canvas.getContext("experimental-webgl", {preserveDrawingBuffer: true});
 	if (!gl) {
 		console.log('gl init fail');
 		return;
@@ -43,4 +45,53 @@ function main(){
     resetGame();
     //控制游戏结束
     loop();
+
+
+    var saveButton = document.getElementById("saveImageBtn");
+    bindButtonEvent(saveButton, "click", saveImageInfo);
+    var dlButton = document.getElementById("downloadImageBtn");
+    bindButtonEvent(dlButton, "click", saveAsLocalImage);
+
 }
+function bindButtonEvent(element, type, handler)
+{
+    if(element.addEventListener) {
+        element.addEventListener(type, handler, false);
+    } else {
+        element.attachEvent('on'+type, handler);
+    }
+}
+
+function saveImageInfo ()
+{
+    var myCanvas = document.getElementById("webgl");
+    // here is the most important part because if you dont replace you will get a DOM 18 exception.
+    // var image = myCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream;Content-Disposition: attachment;filename=foobar.png");
+    var image = myCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    window.location.href=image; // it will save locally
+
+
+}
+
+function saveAsLocalImage () {
+
+    html2canvas(document.body, {
+        allowTaint: true,
+        taintTest: false,
+        onrendered: function(canvas) {
+            canvas.id = "mycanvas";
+            //document.body.appendChild(canvas);
+            //生成base64图片数据
+           var  dataUrl = canvas.toDataURL();
+            var newImg = document.createElement("img");
+            newImg.src =  dataUrl;
+            document.body.appendChild(newImg);
+            document.querySelector(".down").setAttribute('href',canvas.toDataURL());
+        }
+
+
+
+    });
+
+}
+
